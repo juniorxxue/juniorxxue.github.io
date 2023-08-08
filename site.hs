d8ed42afd8d88f2bf01a46309e9733c3d6020243
Xu Xue <juniorxxue@gmail.com>
2023-08-08 16:51:26 +0800

    edits

--- site.hs
+++ site.hs
@@ -91,6 +91,6 @@ main = hakyllWith config $ do
 --------------------------------------------------------------------------------
 postCtx :: Context String
 postCtx =
-    dateField "date" "%B %e, %Y" `mappend`
+    dateField "date" "%Y-%m-%d" `mappend`
     defaultContext
 
